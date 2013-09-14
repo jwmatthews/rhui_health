d309bbcc8a7a151a9db60df38f66e9d20d06fd0c
John Matthews <jwmatthews@gmail.com>
2013-09-14 11:18:01 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RhuiHealth::Application.config.secret_token = 'a8bb85ceafc878c4a325d5232440dbcc4b73ab033a949ed03b0703a12c88a8439a93d0707d5645c5da36f6804103896633f25d2191c49a1939d97b2dfa5130c2'
